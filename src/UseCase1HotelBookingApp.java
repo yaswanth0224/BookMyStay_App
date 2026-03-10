50787a03162db4bcec0a484e6f87169845c7e827
yaswanth0224 <cheepuriyaswanth0224@gmail.com>
2026-03-10 14:38:47 +0530

    Use Case 1 - Application Entry & Welcome Message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,17 @@
+/**
+ * =============================================================
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * =============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
+}
